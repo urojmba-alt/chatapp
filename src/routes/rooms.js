88f8dda25231213ca8943e7e96aad49146d89c81
urojmba-alt <urojmba@gmail.com>
2026-06-06 20:24:15 +0000

    fix horoscope feed - hindustan times

--- src/routes/rooms.js
+++ src/routes/rooms.js
@@ -64,7 +64,7 @@ router.get('/news/:roomId', async (req, res) => {
     movies: 'https://www.hollywoodreporter.com/feed/',
     fitness: 'https://www.menshealth.com/rss/all.xml',
     careers: 'https://www.theguardian.com/careers/rss',
-    horoscope: 'https://feeds.feedburner.com/astrologyzone',
+    horoscope: 'https://www.hindustantimes.com/rss/horoscope/rssfeed.xml',
     relationships: 'https://hollywoodlife.com/feed/',
     makefriends: 'https://pagesix.com/feed/',
     heartbroken: 'https://hollywoodlife.com/feed/',
