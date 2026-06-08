77d01dfe63ec396698e4a1225bc6a0c7e7b6db55
urojmba-alt <urojmba@gmail.com>
2026-06-08 12:38:23 +0000

    fix CORS allow x-auth-token header

--- src/server.js
+++ src/server.js
@@ -57,7 +57,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", origin || "*");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization,Cookie");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization,Cookie,x-auth-token");
   if (req.method === "OPTIONS") return res.sendStatus(200);
   next();
 });
